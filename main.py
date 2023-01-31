68a0276c9e78a8a7f3f4bcb9686a872ec061669d
Alexkkir <alexkkir@gmail.com>
2023-01-31 17:09:39 +0300

    changed main.py

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 def main():
     opts = lib.get_args()
 
-    loaders = lib.get_loaders(opts)
-
+    loaders = lib.get_loaders(opts, fast=True)
     validation_batch = next(iter(loaders['valid']))
+
     model = lib.Model(opts, validation_batch=validation_batch)
 
     if opts['wandb']:
         wandb.init(
+            entity='alexkkir',
             project='IQA', 
             name=opts['name'],
             config=opts
@@ -26,10 +27,15 @@ def main():
     else:
         logger = None
 
-    MyModelCheckpoint = ModelCheckpoint(
+    CheckpointLast = ModelCheckpoint(
         dirpath=f"checkpoints/{opts['name']}/",
-        filename=f'date={lib.today()}_' + '{val_srocc:.3f}_{epoch}',
-        **opts['model_checkoint'])
+        filename=f'last_date={lib.today()}_' + '{val_srocc:.3f}_{epoch}',
+        **opts['model_checkoint_last'])
+
+    CheckpointBest = ModelCheckpoint(
+        dirpath=f"checkpoints/{opts['name']}/",
+        filename=f'best_date={lib.today()}_' + '{val_srocc:.3f}_{epoch}',
+        **opts['model_checkoint_best'])
 
     MyEarlyStopping = EarlyStopping(**opts['early_stopping'])
 
@@ -38,17 +44,23 @@ def main():
         max_epochs=20,
         accelerator='gpu',
         devices=[opts['device']],
-        callbacks=[MyEarlyStopping, MyModelCheckpoint],
+        callbacks=[MyEarlyStopping, CheckpointLast, CheckpointBest],
         log_every_n_steps=1,
     )
 
     trainer.fit(model, loaders['train'], loaders['valid'])
 
-    model.test_dashboard = 'test_koniq'
-    trainer.test(model, loaders['train_koniq'])
+    model.test_dashboard = 'test_koniq_best'
+    trainer.test(ckpt_path='best', dataloaders=loaders['train_koniq'])
 
-    model.test_dashboard = 'test_clive'
-    trainer.test(model, loaders['test_clive'])
+    model.test_dashboard = 'test_koniq_last'
+    trainer.test(ckpt_path='last', dataloaders=loaders['train_koniq'])
+
+    model.test_dashboard = 'test_clive_best'
+    trainer.test(ckpt_path='best', dataloaders=loaders['test_clive'])
+
+    model.test_dashboard = 'test_clive_last'
+    trainer.test(ckpt_path='last', dataloaders=loaders['test_clive'])
 
 if __name__ == '__main__':
     main()
